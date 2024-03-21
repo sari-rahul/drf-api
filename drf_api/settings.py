94a58c8f75cbc83e632491ab0e1c8746bf90f043
Sarimol Adampurath <sarisivadas93@gmail.com>
2024-03-21 13:32:29 +0000

    Change allowed user to live url

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -64,7 +64,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-sarirahul-drfapi-4qewqjucsyd.ws-eu110.gitpod.io','djangorest-framework-api.herokuapp.com']
+ALLOWED_HOSTS = ['8000-sarirahul-drfapi-4qewqjucsyd.ws-eu110.gitpod.io','djangorest-framework-api-b7115ada88d2.herokuapp.com/']
 
 
 # Application definition
