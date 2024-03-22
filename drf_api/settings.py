1e2bf346fedd50cc4195b3195da08b73a88a71b0
Sarimol Adampurath <sarisivadas93@gmail.com>
2024-03-22 09:39:43 +0000

    Change allowed host for delpoyment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -65,7 +65,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-sarirahul-drfapi-4qewqjucsyd.ws-eu110.gitpod.io','os.environ.get("ALLOWED_HOST")',]
+ALLOWED_HOSTS = ['8000-sarirahul-drfapi-4qewqjucsyd.ws-eu110.gitpod.io',os.environ.get("ALLOWED_HOST"),]
 
 
 # Application definition
