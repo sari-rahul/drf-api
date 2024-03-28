f6b882e8a6f3e8cbc58c316aa978f34b7e9b2f71
Sarimol Adampurath <sarisivadas93@gmail.com>
2024-03-28 10:07:32 +0000

    Cross checked the views.py for JWT tokens

--- drf_api/views.py
+++ drf_api/views.py
@@ -1,7 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .settings import (
-    JWT_AUTH_COKKIE,JWT_AUTH_REFRESH_COKKIE,JWT_AUTH_SAMESITE,JWT_AUTH_SECURE,
+    JWT_AUTH_COKKIE,JWT_AUTH_REFRESH_COKKIE,JWT_AUTH_SAMESITE,
+    JWT_AUTH_SECURE,
 )
 @api_view()
 def root_route(request):
